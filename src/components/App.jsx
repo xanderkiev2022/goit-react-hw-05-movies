95a91dcce53cf429670e3eb6949b13c40c78dc55
xanderkiev2022 <301019a@gmail.com>
2023-01-29 16:45:08 +0200

    App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import { Route, Routes } from 'react-router-dom';
+import { SharedLayout } from './SharedLayout';
+import { Home } from '../pages/Home';
+import { Movies } from '../pages/Movies';
+import { MovieDetails } from '../pages/MovieDetails';
+import { Cast } from '../pages/Cast';
+import { Reviews } from '../pages/Reviews';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 };
+
+// '/' – компонент Home, домашня сторінка зі списком популярних кінофільмів.
+// '/movies' – компонент Movies, сторінка пошуку кінофільмів за ключовим словом.
+// '/movies/:movieId' – компонент MovieDetails, сторінка з детальною інформацією про кінофільм.
+// /movies/:movieId/cast – компонент Cast, інформація про акторський склад. Рендериться на сторінці MovieDetails.
+// /movies/:movieId/reviews – компонент Reviews, інформація про огляди. Рендериться на сторінці MovieDetails.
